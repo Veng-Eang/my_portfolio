8d9b73c04f575baa04a34ac74c4743a83418de7e
Veng-Eang <oengvengeang@gmail.com>
2024-04-25 23:36:04 -0500

    contact page for mobile

--- lib/mobile/contact_mobile.dart
+++ lib/mobile/contact_mobile.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:portfolio/components.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactMobile extends StatefulWidget {
   const ContactMobile({super.key});
@@ -8,8 +11,139 @@ class ContactMobile extends StatefulWidget {
 }
 
 class _ContactMobileState extends State<ContactMobile> {
+  Widget _urlLauncher(String imagePath, String url) {
+    return IconButton(
+      icon: SvgPicture.asset(
+        imagePath,
+        width: 35,
+        color: Colors.black,
+      ),
+      onPressed: () async {
+        await launchUrl(Uri.parse(url));
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.white,
+      endDrawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const DrawerHeader(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: CircleAvatar(
+                radius: 117.0,
+                backgroundColor: Colors.tealAccent,
+                child: CircleAvatar(
+                  radius: 110.0,
+                  backgroundColor: Colors.white,
+                  backgroundImage: AssetImage('assets/image-2.png'),
+                ),
+              ),
+            ),
+            TabsMobile(text: 'Home', route: '/'),
+            SizedBox(height: 20),
+            TabsMobile(text: 'Works', route: '/works'),
+            SizedBox(height: 20),
+            TabsMobile(text: 'Blog', route: '/blog'),
+            SizedBox(height: 20),
+            TabsMobile(text: 'About', route: '/about'),
+            SizedBox(height: 20),
+            TabsMobile(text: 'Contact', route: '/contact'),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _urlLauncher(
+                  'assets/instagram.svg',
+                  'https://www.instagram.com/veng_eang',
+                ),
+                _urlLauncher(
+                  'assets/github.svg',
+                  'https://github.com/Veng-Eang',
+                ),
+                _urlLauncher(
+                  'assets/twitter.svg',
+                  'https://twitter.com/o_vengeang',
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                expandedHeight: 400.0,
+                backgroundColor: Colors.white,
+                iconTheme: IconThemeData(
+                  size: 35.0,
+                  color: Colors.black,
+                ),
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Image.asset(
+                    "assets/contact_image.jpg",
+                    filterQuality: FilterQuality.high,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              )
+            ];
+          },
+          body: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(vertical: 25.0),
+            child: Wrap(
+              runSpacing: 20.0,
+              spacing: 20.0,
+              alignment: WrapAlignment.center,
+              children: [
+                SansBold("Contact me", 35.0),
+                TextForm(
+                  text: "First Name",
+                  hintText: "Please enter your first name",
+                  containerWidth: deviceWidth / 1.4,
+                ),
+                TextForm(
+                  text: "Last Name",
+                  hintText: "Please enter your last name",
+                  containerWidth: deviceWidth / 1.4,
+                ),
+                TextForm(
+                  text: "Email",
+                  hintText: "Please enter your email",
+                  containerWidth: deviceWidth / 1.4,
+                ),
+                TextForm(
+                  text: "Phone number",
+                  hintText: "Please enter your phone number",
+                  containerWidth: deviceWidth / 1.4,
+                ),
+                TextForm(
+                  text: "Message",
+                  hintText: "Please type your message",
+                  containerWidth: deviceWidth / 1.4,
+                  maxLine: 10,
+                ),
+                MaterialButton(
+                  onPressed: () {},
+                  elevation: 20.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  height: 60.0,
+                  minWidth: deviceWidth / 2.2,
+                  color: Colors.tealAccent,
+                  child: SansBold("Submit", 20.0),
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
